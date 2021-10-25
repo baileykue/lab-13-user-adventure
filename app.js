8ace6e1d0f758fe240a9871603b0ff88c7342d87
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-25 14:27:12 -0700

    added all necessary html and js files and linked them

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-// import functions and grab DOM elements
-
-// initialize global state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+console.log('this is the main page');
