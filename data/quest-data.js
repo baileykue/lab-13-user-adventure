d57e4b4233d940fa533f171e5641f773f3cf7607
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-25 16:15:09 -0700

    adding seed data for quests, images, and font style

---
+++ data/quest-data.js
@@ -0,0 +1,236 @@
+const tavern = {
+    id: 'tavern',
+    title: 'The Local Tavern',
+    map: {
+        top: '',
+        left: ''
+    },
+    image: 'tavern.jpg',
+    icon: 'table.png',
+    description: `
+        You enter the Tavern and it is full of local life, the 
+        bartender greets you and asks what you want. What do you do?
+    `,
+    choices: [{
+        id: 'drink',
+        description: 'Order a drink',
+        result: `
+            You decide to have a drink, but get a little too sloppy, 
+            and the locals mug you and take your money. 
+        `,
+        health: 0,
+        dabloons: -25
+    }, {
+        id: 'fight',
+        description: 'Get rowdy and start a fight with the biggest baddest guy at the bar',
+        result: `
+            You throw a punch at the guy, and it lands on his unflinching face. He 
+            then takes out his knife and cuts off your finger. You struggle but are no match 
+            for his brutal force. 
+        `,
+        health: -30,
+        dabloons: 0
+    }, {
+        id: 'nap',
+        description: 'Take a nap in the corner booth',
+        result: `
+            As you make a dash for the door a giant spider descends and take a bite of flesh,
+            causing 50 hp damage.
+        `,
+        health: +25,
+        gold: 0
+    }]
+};
+
+const dragon = {
+    id: 'dragon',
+    title: 'A Dragon\'s Lair',
+    map: {
+        top: '',
+        left: ''
+    },
+    image: '',
+    icon: 'dragon.png',
+    description: `
+        You enter the Dragon\'s Lair only to be confronted by a hoard of
+        monsters. And they look hungry. What do you do?
+    `,
+    choices: [{
+        id: 'negotiate',
+        description: 'Negotiate with them',
+        result: `
+            Knowing the monsters are not too bright, you offer to go buy them all
+            turkey dinners from the village pub. They give you 35 gold for meals
+            that will never be delivered. I hope you can live with yourself. 
+        `,
+        hp: 0,
+        gold: 35
+    }, {
+        id: 'fight',
+        description: 'Fiiiiiggghhhttt!',
+        result: `
+            Brandishing your sword you let out a warrior's cry and charge into the monsters
+            hacking and slashing. Before long you stand panting gazing across the bodies of
+            your vanquished foes. The bad news is you take 30 hp damage. The good news is you
+            find 50 gold.
+        `,
+        hp: -30,
+        gold: 50
+    }, {
+        id: 'run',
+        description: 'Run away like good Sir Robin',
+        result: `
+            As you make a dash for the door a giant spider descends and take a bite of flesh,
+            causing 50 hp damage.
+        `,
+        hp: -50,
+        gold: 0
+    }]
+};
+
+const monsters = {
+    id: 'monsters',
+    title: 'A Den of Monsters',
+    map: {
+        top: '89%',
+        left: '44%'
+    },
+    image: 'monsters.jpg',
+    description: `
+        You enter the quest chamber only to be confronted by a hoard of
+        monsters. And they look hungry. What do you do?
+    `,
+    choices: [{
+        id: 'negotiate',
+        description: 'Negotiate with them',
+        result: `
+            Knowing the monsters are not too bright, you offer to go buy them all
+            turkey dinners from the village pub. They give you 35 gold for meals
+            that will never be delivered. I hope you can live with yourself. 
+        `,
+        hp: 0,
+        gold: 35
+    }, {
+        id: 'fight',
+        description: 'Fiiiiiggghhhttt!',
+        result: `
+            Brandishing your sword you let out a warrior's cry and charge into the monsters
+            hacking and slashing. Before long you stand panting gazing across the bodies of
+            your vanquished foes. The bad news is you take 30 hp damage. The good news is you
+            find 50 gold.
+        `,
+        hp: -30,
+        gold: 50
+    }, {
+        id: 'run',
+        description: 'Run away like good Sir Robin',
+        result: `
+            As you make a dash for the door a giant spider descends and take a bite of flesh,
+            causing 50 hp damage.
+        `,
+        hp: -50,
+        gold: 0
+    }]
+};
+
+const dragon = {
+    id: 'dragon',
+    title: 'A HAIRY!!! Dragon',
+    map: {
+        top: '17%',
+        left: '37%'
+    },
+    image: 'dragon.jpg',
+    audio: 'dragon.wav',
+    action: 'dragon-growl.aiff',
+    description: `
+        You run to a nearby village you have heard is being
+        terrorized by a dragon. Sure enough as you rent a room
+        in a local inn, you go outside and see the dragon about
+        to lay seige! What do you do?
+    `,
+    choices: [{
+        id: 'run',
+        description: 'Get the hell out of the village',
+        result: `
+            You high tail it in the opposite direction. Luckily,
+            in the panic you find a bag on the ground with 15 gold.
+            Unluckily, you trip over a discarded wagon wheel on your
+            way out of town and take 40 hp damage. 
+        `,
+        hp: -35,
+        gold: 15
+    }, {
+        id: 'fight',
+        description: 'Fiiiiiggghhhttt!',
+        result: `
+            You attempt to charge towards the dragon, who sees you approach
+            and let's loose a fireball. You wake up the next morning and the
+            village has been completely burned to the ground.
+            Oh, and you take 45 hp damage.
+        `,
+        hp: -45,
+        gold: 0
+    }, {
+        id: 'archer',
+        description: 'Emulate that guy from LOR who shot an arrow',
+        result: `
+            Inspired by the legend of Bard the Bowman, you notice a
+            stunned archer standing nearby and take their bow and quiver,
+            climb to the top of a tall tower and take aim. On the dragon's
+            next pass you steady your aim and let one fly. Amazingly,
+            you strike the dragon in the eye, piercing into the brain and
+            killing the dragon instantly. The villagers declare you their hero
+            and award you 90 gold.
+        `,
+        hp: 0,
+        gold: 90
+    }]
+};
+
+const treasure = {
+    id: 'treasure',
+    title: 'A Golden Treasure',
+    map: {
+        top: '31%',
+        left: '5%'
+    },
+    prerequisites: ['dragon', 'monsters'],
+    image: 'treasure-chests.png',
+    audio: 'treasure-chests.wav',
+    action: 'chest-opening.wav',
+    description: `
+        As you enter the quest chamber you notice three chests before you.
+        Just as you start to imagine the wealth, you see a giant serpent
+        emerge from the back of the chamber. You'll need to make a run for it,
+        but you have time to open one chest before you take off. Which one 
+        do you choose?
+    `,
+    choices: [{
+        id: 'wooden',
+        description: 'A Wooden Chest',
+        result: 'You grab 40 gold pieces!',
+        hp: 0,
+        gold: 40
+    }, {
+        id: 'golden',
+        description: 'A Golden Chest',
+        result: 'Oh no! The chest is booby trapped with poison and you take 50 hp damage',
+        hp: -50,
+        gold: 0
+    }, {
+        id: 'jeweled',
+        description: 'A Jeweled Chest',
+        result: 'A warm light engulfs you and you gain 35 hp',
+        hp: 35,
+        gold: 0
+    }]
+};
+
+const quests = [
+    tavern,
+    dragon,
+    
+];
+
+export default quests;
